c82a8b2912ade6311076c2308f4d1c6988f772da
Lyan-ing <jinhexi2@gmail.com>
2023-11-21 09:47:25 +0800

    支持自定義數據集的dataset builder

--- u2pl/dataset/builder.py
+++ u2pl/dataset/builder.py
@@ -1,7 +1,7 @@
 import logging
 
 from .cityscapes import build_city_semi_loader, build_cityloader
-from .pascal_voc import build_voc_semi_loader, build_vocloader
+from .pascal_voc import build_voc_semi_loader, build_vocloader, build_costum_loader, build_costum_semi_loader
 
 logger = logging.getLogger("global")
 
@@ -31,12 +31,26 @@ def get_loader(cfg, seed=0):
         logger.info("Get loader Done...")
         return train_loader_sup, train_loader_unsup, val_loader
 
+    elif cfg_dataset["type"] == "costum_semi":
+        train_loader_sup, train_loader_unsup = build_costum_semi_loader(
+            "train", cfg, seed=seed
+        )
+        val_loader = build_costum_loader("val", cfg)
+        logger.info("Get loader Done...")
+        return train_loader_sup, train_loader_unsup, val_loader
+
     elif cfg_dataset["type"] == "pascal":
         train_loader_sup = build_vocloader("train", cfg, seed=seed)
         val_loader = build_vocloader("val", cfg)
         logger.info("Get loader Done...")
         return train_loader_sup, val_loader
 
+    # 實現自定義數據集加載模式
+    elif cfg_dataset["type"] == "costum":
+        train_loader_sup = build_costum_loader("train", cfg, seed=seed)
+        val_loader = build_costum_loader("val", cfg)
+        logger.info("Get loader Done...")
+        return train_loader_sup, val_loader
     else:
         raise NotImplementedError(
             "dataset type {} is not supported".format(cfg_dataset)
