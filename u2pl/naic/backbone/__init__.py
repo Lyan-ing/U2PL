764c5a792fb9015526d02e14a19806b644a78616
Lyan-ing <jinhexi2@gmail.com>
2023-11-30 14:26:58 +0800

    naic部分，deeplabv3相关

---
+++ u2pl/naic/backbone/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from ..sync_batchnorm.batchnorm import SynchronizedBatchNorm2d as SyncBN2d
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+# from .senet import se_resnet50, se_resnet101, se_resnet152, se_resnext50_32x4d, se_resnext101_32x4d, senet154
+# from .mobilenet import mobilenet_v2
+# from .shufflenet import shufflenet_v2
+# from .xception import AlignedXception
+# from .resnest import ResNeSt
+_all_resnet_backbones = ['resnet18', 'resnet34', 'resnet50', 'resnet101',
+                         'resnet152']
+
+# _all_resnet_backbones = ['resnet18', 'resnet34', 'resnet50', 'resnet101',
+#                          'resnet152', 'senet154', 'se_resnet50', 'se_resnet101',
+#                          'se_resnet152', 'se_resnext50_32x4d', 'se_resnext101_32x4d']
+
+# _all_mobilenet_backbones = ['mobilenet_v2']
+#
+# _all_shufflenet_backbones = ['shufflenet_v2']
+#
+# _all_xception_backbones = ['AlignedXception']
+#
+# _all_resnest_backbones = ['resnest50', 'resnest101']
