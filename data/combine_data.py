0d0fd5ff22ea007f04285dc54383f3858de4600a
Lyan-ing <jinhexi2@gmail.com>
2023-12-07 09:44:08 +0800

    dataset split and merge

---
+++ data/combine_data.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+
+# 源文件夹路径
+source_folder = r"D:\DQ\train1"
+
+# 目标文件夹路径
+target_folder = r"D:\DQ"
+ann_path = os.path.join(target_folder, "anno")
+jpg_path = os.path.join(target_folder, "jpg")
+os.makedirs(ann_path, exist_ok=True)
+os.makedirs(jpg_path, exist_ok=True)
+# 遍历源文件夹下的子文件夹
+for subfolder in os.listdir(source_folder):
+    subfolder_path = os.path.join(source_folder, subfolder)
+    if not os.path.isdir(subfolder_path):
+        continue
+
+    # 获取ann文件夹和jpg文件夹的路径
+    ann_folder = os.path.join(subfolder_path, "anno")
+    jpg_folder = os.path.join(subfolder_path, "jpg")
+
+
+    # 遍历ann文件夹中的文件
+    for file_name in os.listdir(ann_folder):
+        if not file_name.endswith(".png"):
+            print("-----------------------------")
+            continue
+
+        # 构建新的文件名，加上原子文件夹的名字前缀
+        new_file_name = subfolder + "_" + file_name
+
+        # 源文件路径和目标文件路径
+        src_file = os.path.join(ann_folder, file_name)
+        dst_file = os.path.join(ann_path, new_file_name)
+
+        # 复制文件到目标文件夹
+        shutil.copy(src_file, dst_file)
+        shutil.copy(src_file.replace("anno", "jpg").replace("png", "jpg"), dst_file.replace("anno", "jpg").replace("png", "jpg"))  # anno-->jpg png-->jpg
+
+    # # 遍历jpg文件夹中的文件
+    # for file_name in os.listdir(jpg_folder):
+    #     if not file_name.endswith(".jpg"):
+    #         continue
+    #
+    #     # 构建新的文件名，加上原子文件夹的名字前缀
+    #     new_file_name = subfolder + "_" + file_name
+    #
+    #     # 源文件路径和目标文件路径
+    #     src_file = os.path.join(jpg_folder, file_name)
+    #     dst_file = os.path.join(target_folder, "jpg", new_file_name)
+    #
+    #     # 复制文件到目标文件夹
+    #     shutil.copy(src_file, dst_file)
--- data/write_txt.py
+++ data/write_txt.py
@@ -1,21 +1,35 @@
 # --coding:utf-8--
-import os
+import os, glob
 import random
+
+def copy_first_100_lines(input_file, output_file):
+    with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
+        for i, line in enumerate(in_file):
+            if i < 20000:
+                out_file.write(line)
+            else:
+                break
+
+# 使用函数
+
+
+
 split_rate = 0.8
 unlabel_root = ''
 
-root = r'E:\python\ZEV\U2PL\data\st\train'
+root = r'E:\python\ZEV\U2PL\data\crop5'
 save_path= root
-data_names = os.listdir(os.path.join(root, "jpg"))
-unlabel_data_names = os.listdir(os.path.join(root, "image_A"))
+# copy_first_100_lines(os.path.join(save_path, 'unlabel.txt'), os.path.join(save_path, 'unlabel1.txt'))
+data_names =  os.listdir(os.path.join(root, "jpg"))
+# unlabel_data_names = os.listdir(os.path.join(root, "image_A"))
 # data_names = [i for i in data_names]
 random.shuffle(data_names)
-random.shuffle(unlabel_data_names)
+# random.shuffle(unlabel_data_names)
 split_index = int(len(data_names) * split_rate)
 train_lines = data_names[:split_index]
 val_lines = data_names[split_index:]
-with open(os.path.join(save_path, 'unlabel.txt'), 'w') as f:
-    f.write('\n'.join(unlabel_data_names))
+# with open(os.path.join(save_path, 'unlabel.txt'), 'w') as f:
+#     f.write('\n'.join(unlabel_data_names))
 
 with open(os.path.join(save_path, 'train.txt'), 'w') as f:
     f.write('\n'.join(train_lines))
